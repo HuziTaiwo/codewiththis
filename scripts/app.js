1e9aa9730df681279b72215246a73b83a568c222
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-09 01:11:23 +0100

    updating icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,22 @@
-// db.collection('music').get().then(snapshot => {
-//     // data
-//     snapshot.docs.forEach(doc => {
-//         console.log(doc.data());
-//     })
-// }).catch(err => {
-//     console.log(err);
-// });
+const list = document.querySelector('ul');
+
+const addMusic = (music => {
+    let html = `
+        <li>${music.title}</li>
+        <img src=${music.album_cover} />
+        <audio src=${music.song}></audio>
+    `
+    list.innerHTML += html;
+});
+db.collection('music').get().then(snapshot => {
+    // data
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+        // addMusic(doc.data())
+    })
+}).catch(err => {
+    console.log(err);
+});
 
 // reference from the DOM
 const library = document.querySelector('aside');
