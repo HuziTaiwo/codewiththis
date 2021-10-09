664efe4627381780af289b6899efdf28bd2e2291
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-09 15:22:17 +0100

    getting the music from firebase firestore database

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,29 @@
-const list = document.querySelector('ul');
 
-const addMusic = (music => {
-    let html = `
-        <li>${music.title}</li>
-        <img src=${music.album_cover} />
-        <audio src=${music.song}></audio>
-    `
-    list.innerHTML += html;
-});
-db.collection('music').get().then(snapshot => {
-    // data
-    snapshot.docs.forEach(doc => {
-        console.log(doc.data());
-        // addMusic(doc.data())
-    })
-}).catch(err => {
-    console.log(err);
-});
+// const updateUI = async (music) => {
+//     console.log(music.title);
+// };
+// db.collection('music').get().then(snapshot => {
+//     // data
+//     snapshot.docs.forEach(doc => {
+//         console.log(doc.data());
+//         updateUI(doc.data())
+//     })
+// }).catch(err => {
+//     console.log(err);
+// });
+
+const updateUI = async (musics) => {
+    console.log(musics);
+}
+
+const getMusic = async () => {
+    const response = await db.collection('music').get();
+    return response;
+}
+
+// getMusic()
+//     .then(snapshot => updateUI(snapshot.docs))
+//     .catch(err => console.log(err));
 
 // reference from the DOM
 const library = document.querySelector('aside');
