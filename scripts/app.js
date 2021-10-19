1a76f3dca24588e7d1c594460e07b5c8341c21bc
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-19 07:44:20 +0100

    updating UI

--- scripts/app.js
+++ scripts/app.js
@@ -16,6 +16,9 @@ const library = document.querySelector('aside');
 const libraryBtn = document.querySelector('nav button');
 const playSong = document.querySelector('aside button');
 const play = document.querySelector('aside audio');
+const albumCover = document.querySelector('.album_cover');
+const artistPreview = document.querySelector('.artist');
+const titlePreview = document.querySelector('.title');
 
 playSong.addEventListener('click', () =>{
     play.play();
@@ -29,7 +32,7 @@ const updateUI = async (musics) => {
     musics.forEach(music => {
         const { song, title, album_cover, artist, love  } = music.data();
         library.innerHTML += `
-            <button>
+            <button class="music">
                 <img src=${album_cover} alt="">
                 <div class="details">
                     <h3 class="name">${title}</h3>
@@ -38,6 +41,18 @@ const updateUI = async (musics) => {
                 <audio src=${song}></audio>
             </button>
         `;
+        const musicsData = document.querySelectorAll('.music');
+        const audios = document.querySelectorAll('.music audio');
+
+        musicsData.forEach((musicData, i) => {
+            musicData.addEventListener('click', () => {
+                audios[i].currentTime = 0;
+                audios[i].play();
+            });
+            albumCover.src = `${album_cover[i]}`;
+            artistPreview.textContent = `${artist[i]}`;
+            titlePreview.textContent = `${title[i]}`;
+        });
     });
 }
 
@@ -46,6 +61,6 @@ const getMusic = async () => {
     return response;
 }
 
-getMusic()
-    .then(snapshot => updateUI(snapshot.docs))
-    .catch(err => console.log(err));
+// getMusic()
+//     .then(snapshot => updateUI(snapshot.docs))
+//     .catch(err => console.log(err));
